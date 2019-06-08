cce423b64d15aa210c81895cc6da59d48087ef05
Austin Smith <austin.smith1998@hotmail.com>
2019-06-08 13:01:24 -0400

    Added include file to include all fight states.

---
+++ fightgame/all_fight_states.h
@@ -0,0 +1,11 @@
+#ifndef _ALL_FIGHT_STATES_H
+#define _ALL_FIGHT_STATES_H
+
+#include "fighter_state.h"
+#include "crouch_state.h"
+#include "fightmove_state.h"
+#include "idle_state.h"
+#include "move_left_state.h"
+#include "move_right_state.h"
+
+#endif
