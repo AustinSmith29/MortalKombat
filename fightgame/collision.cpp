38adbed43bfb265410f2d4e88313a229295473ac
Austin Smith <austin.smith1998@hotmail.com>
2019-07-22 17:35:00 -0400

    Created seperate function resolve_hitbox_hitbox_collision

--- fightgame/collision.cpp
+++ fightgame/collision.cpp
@@ -72,33 +72,15 @@ std::vector<SDL_Rect> get_fighter_dmgboxes(Fighter& fighter)
 	return dmgboxes;
 }
 
-static int get_largest_overlap_x(const SDL_Rect& a, const SDL_Rect& b);
+static void resolve_hitbox_hitbox_collision(Fighter& subject, Fighter& other);
 static int get_largest_overlap_x(const std::vector<SDL_Rect>& a, const std::vector<SDL_Rect>& b);
+static int get_largest_overlap_x(const SDL_Rect& a, const SDL_Rect& b);
 void handle_fighter_on_fighter_collision(Fighter& subject, Fighter& other)
 {
-	auto opponent_dmgboxes = get_fighter_dmgboxes(other);
 	auto opponent_hitboxes = get_fighter_hitboxes(other);
 	if (is_fighter_hitbox_collision_in(subject, opponent_hitboxes))
 	{
-		// TODO: Move this to its own function
-		auto subject_hitboxes = get_fighter_hitboxes(subject);
-		int dx = get_largest_overlap_x(subject_hitboxes, opponent_hitboxes);
-		int subject_vel_x = subject.get_velocity_x();
-		int other_vel_x = other.get_velocity_x();
-		if (subject_vel_x > other_vel_x)
-		{
-			other.set_position_x(other.get_position_x() + dx);
-		}
-		else if (subject_vel_x < other_vel_x)
-		{
-			other.set_position_x(other.get_position_x() - dx);
-		}
-		else
-		{
-			subject.set_velocity_x(0);
-			other.set_velocity_x(0);
-			//subject.set_position_x(subject.get_position_x() + dx);
-		}
+		resolve_hitbox_hitbox_collision(subject, other);
 	}
 	/*
 	if (is_fighter_hitbox_collision_in(subject, opponent_dmgboxes))
@@ -107,6 +89,29 @@ void handle_fighter_on_fighter_collision(Fighter& subject, Fighter& other)
 	}
 	*/
 }
+void resolve_hitbox_hitbox_collision(Fighter& subject, Fighter& other)
+{
+	auto subject_hitboxes = get_fighter_hitboxes(subject);
+	auto opponent_hitboxes = get_fighter_hitboxes(other);
+
+	int dx = get_largest_overlap_x(subject_hitboxes, opponent_hitboxes);
+	int subject_vel_x = subject.get_velocity_x();
+	int other_vel_x = other.get_velocity_x();
+	if (subject_vel_x > other_vel_x)
+	{
+		other.set_position_x(other.get_position_x() + dx);
+	}
+	else if (subject_vel_x < other_vel_x)
+	{
+		other.set_position_x(other.get_position_x() - dx);
+	}
+	else
+	{
+		subject.set_velocity_x(0);
+		other.set_velocity_x(0);
+	}
+}
+
 int get_largest_overlap_x(const std::vector<SDL_Rect>& a, const std::vector<SDL_Rect>& b)
 {
 	assert(intersects(a, b));
