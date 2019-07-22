fb723cbdaf8664774fb7ec9f9548284bc832bda5
Austin Smith <austin.smith1998@hotmail.com>
2019-07-22 17:13:41 -0400

    Added dummy ai for ease of testing.

---
+++ fightgame/dummy_ai.cpp
@@ -0,0 +1,6 @@
+#include "dummy_ai.h"
+
+void DummyAI::logic(AIFighter& ai, Fighter& other)
+{
+
+}
---
+++ fightgame/dummy_ai.h
@@ -0,0 +1,11 @@
+#ifndef _DUMMY_AI_H
+#define _DUMMY_AI_H
+
+class AIFighter;
+class Fighter;
+namespace DummyAI
+{
+	void logic(AIFighter& ai, Fighter& other);
+}
+
+#endif
