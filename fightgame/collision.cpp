79b6e0daa8cbd862b9941425924f3cbbd92c8dbd
Austin Smith <austin.smith1998@hotmail.com>
2019-07-22 16:29:39 -0400

    KISS on collision detection.

--- fightgame/collision.cpp
+++ fightgame/collision.cpp
@@ -14,60 +14,46 @@ static void convert_coords(std::vector<SDL_Rect>& boxes, int x, int y)
 	}
 }
 
-bool intersects(SDL_Rect& a, SDL_Rect& b)
+bool intersects(const SDL_Rect& a, const SDL_Rect& b)
 {
 	return SDL_HasIntersection(&a, &b);
 }
 
-Collider::Collider(Fighter& owner) : fighter(owner)
+bool intersects(const SDL_Rect& a, const std::vector<SDL_Rect>& b)
 {
-}
-
-bool collision_in(std::vector<SDL_Rect>& vec, SDL_Rect& other)
-{
-	for (auto box : vec)
+	for (auto rect : b)
 	{
-		if (intersects(box, other))
+		if (intersects(a, rect))
 			return true;
 	}
 	return false;
 }
 
-bool Collider::hitbox_collision(SDL_Rect& other)
+bool intersects(const std::vector<SDL_Rect>& a, const std::vector<SDL_Rect>& b)
 {
-	auto hitboxes = get_fighter_hitboxes();
-	return collision_in(hitboxes, other);
-}
-
-bool Collider::hitbox_collision(Collider& other)
-{
-	auto other_dmgboxes = other.get_fighter_dmgboxes();
-	for (auto dmgbox : other_dmgboxes)
+	for (auto rect : a)
 	{
-		if (hitbox_collision(dmgbox))
+		if (intersects(rect, b))
 			return true;
 	}
 	return false;
 }
 
-bool Collider::damagebox_collision(SDL_Rect& other)
+static std::vector<SDL_Rect> get_fighter_hitboxes(Fighter& fighter);
+static std::vector<SDL_Rect> get_fighter_dmgboxes(Fighter& fighter);
+static bool is_fighter_hitbox_collision_in(Fighter& fighter, const std::vector<SDL_Rect>& boxes)
 {
-	auto dmgboxes = get_fighter_dmgboxes();
-	return collision_in(dmgboxes, other);
+	auto hitboxes = get_fighter_hitboxes(fighter);
+	return intersects(hitboxes, boxes);
 }
 
-bool Collider::damagebox_collision(Collider& other)
+static bool is_fighter_damagebox_collision_in(Fighter& fighter, const std::vector<SDL_Rect>& boxes)
 {
-	auto other_hitboxes = other.get_fighter_hitboxes();
-	for (auto hitbox : other_hitboxes)
-	{
-		if (damagebox_collision(hitbox))
-			return true;
-	}
-	return false;
+	auto dmgboxes = get_fighter_dmgboxes(fighter);
+	return intersects(dmgboxes, boxes);
 }
 
-std::vector<SDL_Rect> Collider::get_fighter_hitboxes()
+std::vector<SDL_Rect> get_fighter_hitboxes(Fighter& fighter)
 {
 	bones::Frame frame = fighter.get_animation()->get_current_frame();
 	std::vector<SDL_Rect> hitboxes = frame.hitboxes;
@@ -75,7 +61,7 @@ std::vector<SDL_Rect> Collider::get_fighter_hitboxes()
 	return hitboxes;
 }
 
-std::vector<SDL_Rect> Collider::get_fighter_dmgboxes()
+std::vector<SDL_Rect> get_fighter_dmgboxes(Fighter& fighter)
 {
 	bones::Frame frame = fighter.get_animation()->get_current_frame();
 	std::vector<SDL_Rect> dmgboxes = frame.damageboxes;
@@ -83,17 +69,25 @@ std::vector<SDL_Rect> Collider::get_fighter_dmgboxes()
 	return dmgboxes;
 }
 
-void Collider::draw_boxes(SDL_Renderer* renderer)
+void handle_fighter_on_fighter_collision(Fighter& subject, Fighter& other)
+{
+	auto opponent_dmgboxes = get_fighter_dmgboxes(other);
+	if (is_fighter_hitbox_collision_in(subject, opponent_dmgboxes))
+	{
+		subject.set_state(FighterStateMachine::State::BLOCK);
+	}
+}
+
+void draw_fighter_collision_boxes(SDL_Renderer* renderer, Fighter& fighter)
 {
 	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-	auto hitboxes = get_fighter_hitboxes();
+	auto hitboxes = get_fighter_hitboxes(fighter);
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 100);
 	for (auto hit : hitboxes)
 		SDL_RenderFillRect(renderer, &hit);
 
-	auto dmgboxes = get_fighter_dmgboxes();
+	auto dmgboxes = get_fighter_dmgboxes(fighter);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 100);
 	for (auto dmg : dmgboxes)
 		SDL_RenderFillRect(renderer, &dmg);
 }
-
--- fightgame/collision.h
+++ fightgame/collision.h
@@ -4,24 +4,11 @@
 #include <SDL.h>
 #include <vector>
 
-bool intersects(SDL_Rect& a, SDL_Rect& b);
+bool intersects(const SDL_Rect& a, const SDL_Rect& b);
+bool intersects(const SDL_Rect& a, const std::vector<SDL_Rect>& b);
+bool intersects(const std::vector<SDL_Rect>& a, const std::vector<SDL_Rect>& b);
 
 class Fighter;
-class Collider
-{
-public:
-	Collider(Fighter& owner);
-
-	bool hitbox_collision(SDL_Rect& other);
-	bool hitbox_collision(Collider& other);
-
-	bool damagebox_collision(SDL_Rect& other);
-	bool damagebox_collision(Collider& other);
-
-	void draw_boxes(SDL_Renderer* renderer);
-private:
-	Fighter& fighter;
-	std::vector<SDL_Rect> get_fighter_hitboxes();
-	std::vector<SDL_Rect> get_fighter_dmgboxes();
-};
+void handle_fighter_on_fighter_collision(Fighter& subject, Fighter& other);
+void draw_fighter_collision_boxes(SDL_Renderer* renderer, Fighter& fighter);
 #endif
